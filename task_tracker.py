bf0305c06721afb0b8b58e23efe138f602d09551
0xarash <arpachinou02@gmail.com>
2025-12-20 16:19:13 +0330

    task_tracker.py

---
+++ task_tracker.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+DATA_FILE = "tasks.json"
+
+
+def load_tasks():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_tasks(tasks):
+    with open(DATA_FILE, "w") as file:
+        json.dump(tasks, file, indent=4)
+
+
+def add_task(title):
+    tasks = load_tasks()
+    tasks.append({
+        "title": title,
+        "completed": False
+    })
+    save_tasks(tasks)
+    print("Task added successfully.")
+
+
+def list_tasks():
+    tasks = load_tasks()
+    if not tasks:
+        print("No tasks found.")
+        return
