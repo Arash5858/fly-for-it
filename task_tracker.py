9f6f8a3b6d3ee6701cf3180f7f95451ac5e5f7a7
0xarash <arpachinou02@gmail.com>
2026-02-12 16:13:27 +0330

    Update task_tracker.py

--- task_tracker.py
+++ task_tracker.py
@@ -11,6 +11,11 @@ def load_tasks():
         return json.load(file)
 
 
+def load_tasks():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
 def save_tasks(tasks):
     with open(DATA_FILE, "w") as file:
         json.dump(tasks, file, indent=4)
